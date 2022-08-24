ec6b229a8480285e1cc2a6d9ff1676ccede923c8
BillyVII <gabrielasteria@gmail.com>
2022-08-24 11:10:01 +0200

    picker view oki, affichage des storyboard correct

---
+++ iosEval2/iosEval2/ViewController/PokemonTabBarVCViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonTabBarVCViewController.swift
+//  iosEval2
+//
+//  Created by Student09 on 24/08/2022.
+//
+
+import UIKit
+
+class PokemonTabBarVCViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
