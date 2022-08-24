7252928a4c4f56a719f651cf3c223895ac10e6e1
BillyVII <gabrielasteria@gmail.com>
2022-08-24 09:41:57 +0200

    Class PokemonVC, PokemonTVC, creation de la liste des pokemon et affichage

---
+++ iosEval2/iosEval2/ViewCell/PokemonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonTableViewCell.swift
+//  iosEval2
+//
+//  Created by Student09 on 24/08/2022.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
